60935d37d527837d51826f9e10a5a0cbc4dfc0c2
xGoldwarx <goldwar01@hotmail.com>
2018-02-22 23:00:44 -0500

    listas

--- Templates/Principal.php
+++ Templates/Principal.php
@@ -9,20 +9,70 @@
 	<?php include_once("../includes/cabecera.php"); ?> 
 	<?php echo "probando php";  ?>
 
+	    
+<h2>Tecnologias</h2> 
 
-	<body> 
-	    <h2>Tecnologias</h2> 
-	     <div class="content-all"> 
-	         <div class="content-carrousel"> 
-	             <figure><img src="../img/Duck.jpg"></figure> 
-	             <figure><img src="../img/Duck.jpg"></figure> 
-	             <figure><img src="../img/Duck.jpg"></figure> 
-	             <figure><img src="../img/Duck.jpg"></figure> 
-	         </div> 
-	     </div> 
-	</body> 
+<div class="content-all"> 
+	<div class="content-carrousel"> 
+	    <figure><img src="../img/Duck.jpg"></figure> 
+	    <figure><img src="../img/Duck.jpg"></figure> 
+	    <figure><img src="../img/Duck.jpg"></figure> 
+	    <figure><img src="../img/Duck.jpg"></figure> 
+	</div> 
+</div>
+
+<ul class="list">
+  <li class="list-item">
+    <div class="list-content">
+      <h2>Title</h2>
+      <img src="http://baconmockup.com/250/200" alt="" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, eveniet.</p>
+      <a href="">Link</a>
+    </div>
+  </li>
 	
+	 <li class="list-item">
+    <div class="list-content">
+      <h2>Title</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, vel, quia. Non nostrum, consectetur ipsum doloribus enim maiores a laudantium, odio vel blanditiis id ea dolorum expedita fugit incidunt commodi.</p>
+      <a href="">Link</a>
+    </div>
+  </li>
+  
+  <li class="list-item">
+    <div class="list-content">
+      <h2>Title</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, vel, quia. Non nostrum, consectetur ipsum doloribus enim maiores a laudantium.</p>
+      <a href="">Link</a>
+    </div>
+  </li>
+  
+  <li class="list-item">
+    <div class="list-content">
+      <h2>Title</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur optio ipsa fuga vel repudiandae impedit illum delectus nihil error animi nobis quaerat quidem, amet, praesentium aspernatur inventore numquam! Totam, dolorem inventore reprehenderit, culpa obcaecati!</p>
+      <a href="">Link</a>
+    </div>
+  </li>
+  
+  <li class="list-item">
+    <div class="list-content">
+      <h2>Title</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo veniam tempore vero velit deleniti corporis recusandae placeat eum repellendus modi architecto, iste, numquam nulla, praesentium fuga soluta eos consequatur sequi expedita! Ea aspernatur explicabo optio ducimus officia blanditiis voluptas possimus veniam maxime!</p>
+      <a href="">Link</a>
+    </div>
+  </li>
 
+  
+    <li class="list-item">
+    <div class="list-content">
+      <h2>Title</h2>
+      <img src="http://baconmockup.com/250/200" alt="" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum molestiae esse dolore corporis velit fugiat ipsam tempore optio dicta. Magnam iusto facere, maxime! Praesentium quo laborum, deserunt adipisci. Ipsam officia delectus repudiandae, aperiam magnam blanditiis fuga similique.</p>
+      <a href="">Link</a>
+    </div>
+  </li>
+</ul>
 
 </body>
 </html>
